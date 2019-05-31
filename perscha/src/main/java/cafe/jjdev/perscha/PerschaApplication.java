fc3063e4032fe97749f830a28097790895eca123
Administrator <Administrator@MSDN-SPECIAL>
2019-05-31 12:15:39 +0900

    perscha 초기화

---
+++ perscha/src/main/java/cafe/jjdev/perscha/PerschaApplication.java
@@ -0,0 +1,13 @@
+package cafe.jjdev.perscha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerschaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerschaApplication.class, args);
+	}
+
+}
---
+++ perscha/src/test/java/cafe/jjdev/perscha/PerschaApplicationTests.java
@@ -0,0 +1,16 @@
+package cafe.jjdev.perscha;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PerschaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
